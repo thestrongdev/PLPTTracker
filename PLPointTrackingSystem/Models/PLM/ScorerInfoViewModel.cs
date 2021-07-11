0099f4f23b8790988d0f25a31522c04aba5d436f
Candace <goodsoncj@gmail.com>
2021-07-11 12:59:41 -0400

    scorer info form

---
+++ PLPointTrackingSystem/Models/PLM/ScorerInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PLPointTrackingSystem.Models.PLM
+{
+    public class ScorerInfoViewModel
+    {
+
+        public int MemberID { get; set; }
+        public string Role { get; set; }
+    }
+}
